99c10878f5a7e1785bd6b71831f9025b6f4d5d34
Amari1007 <amarighambi@gmail.com>
2025-01-17 15:35:34 +0200

    request, response functionality

--- index.mjs
+++ index.mjs
@@ -2,14 +2,21 @@ import express from "express";
 import path from "path";
 
 const server = express();
-server.use(express.static("public"))
 const PORT = 7341;
 
+server.use(express.json());
+server.use(express.static("public"))
 server.set("view engine", "ejs");
 server.set("views", "views")
 
 server.get("/home", (req, res) => {
     res.status(200).render("home");
+});
+
+server.post("/api/validate_once", (req, res) => {
+    //res.json({status:true}).status(200);
+    console.log(req.body);
+    res.status(200).json({name:"Ghambi"});
 });
 
 //******************DELETE IN PRODUCTION************ */
