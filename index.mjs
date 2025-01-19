b6ead7730ba6cca656d94abf6599bea5c946f96c
Amari1007 <amarighambi@gmail.com>
2025-01-19 22:59:28 +0200

    implemented node-email-verifier

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import emailValidator from 'node-email-verifier';
 import path from "path";
 
 const server = express();
@@ -13,13 +14,27 @@ server.get("/home", (req, res) => {
     res.status(200).render("home");
 });
 
-server.post("/api/validate_once", (req, res) => {
-    //res.json({status:true}).status(200);
-    console.log(req.body);
-    res.status(200).json({name:"Ghambi"});
+server.post("/api/validate_once", async (req, res) => {
+    const email = req.body.email;
+    const validate = await validateEmailWithMx(email.toString());
+    res.status(200).json({data: validate});
 });
 
 //******************DELETE IN PRODUCTION************ */
 server.listen(PORT, () => {
     console.log(`email valid started on port ${PORT}`);
-})
+});
+
+async function validateEmailWithMx(email) {
+    try {
+        const isValid = await emailValidator(email, {checkMx:true});
+        console.log(isValid);
+        return {decision: isValid};
+    } catch (error) {
+        if(error.message.match(/timed out/)){
+            return {decision: false, message: "Timeout on lookup"};
+        }else{
+            return {decision: false, message: "Error validating email"};
+        }
+    }
+}
